e71168ab401914a2439f704a271ddf1eda429009
Henrique Queiroz <zeccakut@gmail.com>
2024-12-04 00:49:19 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/index.html', './src/script.js'],
+  content: ['./src/**/*.{html,js}'],
   theme: {
     extend: {
       colors: {
